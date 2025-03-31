166b4761c4253ae85fd057be1e01cc983a691f29
Maksim Gadzhiusmanov <maximggvp@gmail.com>
2025-03-31 19:25:34 +0300

    Feat: added the dependencies needed for the tests

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -11,22 +11,27 @@ plugins {
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
+    id("jacoco")
 }
 
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
-
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        html.required.set(true)
+    }
+}
 dependencies {
     // Use JUnit Jupiter for testing.
     testImplementation(libs.junit.jupiter)
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
     // This dependency is exported to consumers, that is to say found on their compile classpath.
     api(libs.commons.math3)
-
+    testImplementation("org.assertj:assertj-core:3.24.2")
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     implementation(libs.guava)
 }
